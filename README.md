7b5f214cfeaa25225251ced3089e43475fe0e532
Duane Creates <duanecreates@gmail.com>
2019-02-16 19:56:07 +0100

    Add Vue SPA base

---
+++ README.md
@@ -0,0 +1,8 @@
+# 100 Days of Code
+Please find each day's work in a separate branch
+
+### Clone This Project
+```
+npm install
+npm run serve
+```
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>100 Days of Code</h1>
+    <strong>By Duane Creates</strong>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
