fc0988ba36bd3db332d38d448be2c7196b5d695e
Duane Creates <duanecreates@gmail.com>
2019-02-16 22:50:45 +0100

    Add Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import '@/assets/css/tailwind.css';
 
 import Vue from 'vue';
+import Icon from 'vue-awesome/components/Icon.vue';
 import App from './App.vue';
+
+Vue.component('v-icon', Icon);
 
 Vue.config.productionTip = false;
 
