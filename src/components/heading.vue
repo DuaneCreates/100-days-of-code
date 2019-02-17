986aa1bfdcb66d2a50ef54aa5ca86156ace2ca50
Duane Creates <duanecreates@gmail.com>
2019-02-17 22:36:24 +0100

    Update heading avatar to overlap

--- src/components/heading.vue
+++ src/components/heading.vue
@@ -3,7 +3,6 @@
     <section class="heading">
       <h1 class="title">{{info.title}}</h1>
       <p class="subtitle"><a :href="info.instagram_url">{{info.name}}</a></p>
-      <img class="avatar" src="https://placeimg.com/150/150/tech"/>
       <div class="external">
         <a v-if="info.instagram_url" :href="info.instagram_url" class="external__item">
           <v-icon name="brands/instagram" scale="2.5"/>
@@ -15,6 +14,7 @@
           <v-icon name="brands/codepen" scale="2.5"/>
         </a>
       </div>
+      <img class="avatar" src="https://placeimg.com/150/150/tech"/>
     </section>
   </div>
 </template>
@@ -39,7 +39,7 @@ export default {
     @apply bg-grey-lightest shadow;
 
     .heading {
-      @apply pt-10 pb-7 text-center flex flex-col mb-2 max-w-4xl mx-auto;
+      @apply pt-10 text-center flex flex-col mb-2 max-w-4xl mx-auto;
 
       .title {
         @apply font-montserrat text-4xl text-blue-darkest;
@@ -54,7 +54,7 @@ export default {
       }
 
       .avatar {
-        @apply rounded-full w-32 h-32 mx-auto mt-8 border-blue;
+        @apply rounded-full w-32 h-32 mx-auto mt-7 border-grey-lightest shadow-md border-4 -mb-12;
         object-fit: cover;
       }
 
