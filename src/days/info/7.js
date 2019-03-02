bf74d76e7fa82d2fca9871a9679b6cbd661e5f44
Duane Creates <duanecreates@gmail.com>
2019-03-02 22:46:56 +0100

    Add day 7

---
+++ src/days/info/7.js
@@ -0,0 +1,16 @@
+export default {
+  day: 7,
+  type: 'log',
+  date: '2019-03-02',
+  title: 'Node.js Frameworks - Spoilt for choice',
+  post: `
+  Last time I started to research about Node.js and try some basic examples using its raw form.  
+  Today I tried Express.js, and although it seems very light and fast, I want a framework that does more right off the bat. I come from the Laravel framework mindset, so I was hoping for something similar for Node.js.  
+  Turns out, that there exists a framework called Adonis, which is built on the principles of Laravel, but I still need to look into it more to see whether it is lacking behind, or more advanced, when compared to Laravel.  
+  It seems like theere are other good alternatives as well, some mature choices such as Sails.js and Meteor, and some other lesser known ones such as Feathers.js.  
+  I will need to try more than one of them, because there are many to choose from, which makes it harder to choose!
+  `,
+  tags: ['js', 'nodejs', 'research'],
+  github_url: null,
+  codepen_url: null,
+};
