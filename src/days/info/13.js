c75a7a8d31036749c3cb23c41fe92be4098d7307
Duane Creates <duanecreates@gmail.com>
2019-03-13 21:44:00 +0100

    Add day 13

---
+++ src/days/info/13.js
@@ -0,0 +1,14 @@
+export default {
+  day: 13,
+  type: 'log',
+  date: '2019-03-13',
+  title: 'Pug.js',
+  post: `
+   I've been seeing pug.js (formerly known as Jade) a lot, but I never tried it because I'm used to normal HTML.  
+  Pug is a template engine, to write HTML in a simpler and cleaner way, which will then be compiled into normal HTML. You can do loops and conditions with it, which is similar to using Vue to manipulate the DOM, so it is not that useful when used with something like React or Vue, but it is still useful to create cleaner view code.  
+  I decided to try it out when I was working on the CSS loading animations, because it was using simple HTML structures so I thought it is good to start small. I liked it! But is it worth the extra learning curve? It does produce cleaner code in my opinion, and it is not hard to use, but if you don't feel like learning a new thing just to create HTML, then don't bother. I think I will be using it more in my future works, which will help me decide if it is worth using or not.  
+  `,
+  tags: ['html', 'javascript', 'research'],
+  github_url: null,
+  codepen_url: null,
+};
