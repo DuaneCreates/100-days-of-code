b7c5e0bfe718449e46fdfb3fbf851b8ab2fe7eae
Duane Creates <duanecreates@gmail.com>
2019-03-22 00:04:37 +0100

    Add day 18

---
+++ src/days/info/18.js
@@ -0,0 +1,14 @@
+export default {
+  day: 18,
+  type: 'result',
+  date: '2019-03-21',
+  title: 'Mouse & Spouse',
+  post: `
+  #### JS 30 #016 Challenge  
+  
+  This is a cool effect that I've wanted to learn about for a long time! Thankfully it was part of JS30 so now I can understand how it works. Thanks, Wes.
+  `,
+  tags: ['js', 'js30', 'tailwindcss'],
+  github_url: 'https://github.com/DuaneCreates/100-days-of-code/tree/day/18',
+  codepen_url: null,
+};
