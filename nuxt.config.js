ef7437698790361d14b88df1123c5a32c4443419
Duane Creates <duanecreates@gmail.com>
2019-04-20 22:17:02 +0200

    Add daily components to Purge CSS whitelist

--- nuxt.config.js
+++ nuxt.config.js
@@ -121,7 +121,8 @@ export default {
             paths: glob.sync([
               path.join(__dirname, './pages/**/*.vue'),
               path.join(__dirname, './layouts/**/*.vue'),
-              path.join(__dirname, './components/**/*.vue')
+              path.join(__dirname, './components/**/*.vue'),
+              path.join(__dirname, './days/results/**/*.vue')
             ]),
             extractors: [
               {
