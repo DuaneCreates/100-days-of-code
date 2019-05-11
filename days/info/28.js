94fbad46ab93b4f2b8dc1d4494c25aa03bd8a0b5
Duane Creates <duanecreates@gmail.com>
2019-05-11 12:51:28 +0200

    Add day 28

---
+++ days/info/28.js
@@ -0,0 +1,13 @@
+export default {
+  day: 28,
+  type: 'ui',
+  date: '2019-04-15',
+  title: 'Flexing Flexbox',
+  post: `
+  Flexbox is awesome, probably my favourite part of CSS. It is not hard to understand, it just needs a little effort at first. Once you get the hang of it, you'll be able to place things where you want and lay elements as you imagine.  
+  If you want to create layouts with code, or work in front-end, I really suggest you check it out. It does not need a library, as it comes with normal CSS and is supported by all the major web browsers.  
+  `,
+  tags: ['ui', 'tailwindcss'],
+  github_url: 'https://github.com/DuaneCreates/100-days-of-code/tree/day/28',
+  codepen_url: 'https://codepen.io/duanecreates/pen/GaqpBv'
+}
---
+++ days/results/28.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="flex flex-row flex-wrap bg-orange">
+    <div class="w-1/2 h-32 bg-purple"></div>
+    <div class="w-1/2 h-32 bg-blue"></div>
+    <div class="w-full h-16 bg-black"></div>
+    <div class="w-3/4 h-16 bg-blue"></div>
+    <div class="w-1/4 h-16 bg-red"></div>
+    <div class="w-full h-16 bg-orange"></div>
+    <div class="w-1/4 h-16 bg-red"></div>
+    <div class="w-3/4 h-16 bg-blue"></div>
+    <div class="w-full h-16 bg-black"></div>
+    <div class="w-1/2 h-32 bg-blue"></div>
+    <div class="w-1/2 h-32 bg-purple"></div>
+    <div class="w-full h-16 bg-black"></div>
+    <div class="w-1/4 h-16 bg-orange"></div>
+    <div class="w-1/2 h-16 bg-purple"></div>
+    <div class="w-1/4 h-16 bg-orange"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Day28'
+}
+</script>
