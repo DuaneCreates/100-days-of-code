6004848dcc7ce3ceca6f99869b57405589e5f3f0
Duane Creates <duanecreates@gmail.com>
2019-05-12 22:50:41 +0200

    Add day 33 & 34

---
+++ days/info/33.js
@@ -0,0 +1,15 @@
+export default {
+  day: 33,
+  type: 'log',
+  date: '2019-05-07',
+  title: 'Game Development is not a one-man thing',
+  post: `
+  I was thinking of starting a game in the past few days, and although I had some nice ideas for an original piece, I realized after some prototypes that creating a game alone would take so much time which I don't feel ready to spare.  
+  There's also the problem that usually, a person is not good at all aspects of game development. In my case, the artistic side is lacking as I found it hard to create characters. I tried both 3D and 2D and figured that the best approach would be to team up with an artist.  
+  Teaming up would make everything serious and I'm not ready to do that right now as I want to explore different things and learn more about web development to better equip myself in my main objectives.  
+  This means that for now I will be putting game development on the side and approach it again later on in the future when I can dedicate more time and general commitments to it.  
+  `,
+  tags: ['unity', 'unity3d', 'gamedev'],
+  github_url: null,
+  codepen_url: null
+}
---
+++ days/info/34.js
@@ -0,0 +1,14 @@
+export default {
+  day: 34,
+  type: 'log',
+  date: '2019-05-10',
+  title: 'Vision straight to code - no more',
+  post: `
+  As I am a coder at heart, when I used to have a vision for a piece of UI, I would get straight to coding it.  
+  Lately I've been hearing a lot about [Adobe XD](https://www.adobe.com/mt/products/xd.html), which is a free tool by Adobe to design and prototype user interfaces. I decided to try it out and I liked it! This changed me, as then when I had visions of user interfaces I went straight to designing it in Adobe XD instead. This made me much more creative as I was not limited by code, and dragging and placing components around with a snapping tool makes everything easier to come up with cool designs.  
+  It's always good to keep learning and trying out new things and approaches to keep evolving yourself.  
+  `,
+  tags: ['ui', 'uidesign'],
+  github_url: null,
+  codepen_url: null
+}
